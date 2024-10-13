315f932d370bd623f83e28ebfdae5dc5e58b4aca
sai-kaneko-31 <sai.kaneko.31@gmail.com>
2024-10-13 12:22:58 +0900

    Load URLs from file

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	INPUT_FILE_NAME = "url_list.txt"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func loadFile() []string {
+	file, err := os.Open(INPUT_FILE_NAME)
+	check(err)
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	check(scanner.Err())
+	return lines
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	urls := loadFile()
+	for _, url := range urls {
+		fmt.Println(url)
+	}
 }
